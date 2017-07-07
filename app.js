d9543553051da478427baab85187236af0256640
Iain Majer <MajerI@cardiff.ac.uk>
2017-07-07 11:31:01 +0100

    Error correction, implemented NMasonry, got basic projects view working

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express()
 
 //Create locals for use during app
 app.locals.moment = require('moment')
+var fs = require('fs')
 
 //Site Settings
 var site = require('./site.json')
@@ -13,12 +14,30 @@ app.set('view engine', 'pug')
 
 //Home page
 app.get('/', function (req, res) {
-  res.render('home', {site: site, title:'Home', page:'home'})
+  res.render('home', { site: site, title: 'Home', url: req.url })
 })
 
 //About Me page 
 app.get('/about', function (req, res) {
-  res.render('about', {site: site, title:'About Me', page:'about'})
+  res.render('about', { site: site, title: 'About Me', url: req.url })
+})
+
+//Projects page, displays all projects
+app.get('/projects', function (req, res) {
+  //Scan projects folder to get list of projects
+  fs.readdir('./public/projects', function (error, data) {
+    //Throw error if things go wrong
+    if (error) {
+      res.status(500).send(error);
+      return;
+    }
+    //Get the extra info on each project
+    var projects = {}
+    data.forEach(function (entry) {
+      projects[entry] = require("./public/projects/" + entry + "/info.json")
+    })
+    res.render('projects', { site: site, title: 'My Projects', url: req.url, projects: projects })
+  });
 })
 
 //Static content handling
@@ -27,6 +46,25 @@ app.use('/js', express.static('public/js'))
 app.use('/fonts', express.static('public/fonts'))
 app.use('/images', express.static('public/images'))
 
+//Some 404 handling
+app.get("/*", function (req, res, next) {
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('errors/404', { site: site, title: 'Error', url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
 
 //Set the app to listen
 //TODO: change to better port, or use sockets
--- public/js/init.js
+++ public/js/init.js
@@ -2,10 +2,8 @@
     $(function () {
 
         $('.button-collapse').sideNav({
-            closeOnClick: true,
             draggable: true
         });
-        $('.parallax').parallax();
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
